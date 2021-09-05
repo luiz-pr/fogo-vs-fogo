c1c644a2a9562ff2c3f2f1bea6197c809c9c406f
luiz-pr <76491544+luiz-pr@users.noreply.github.com>
2021-09-05 20:20:44 -0300

    modificando o js

--- js/login.js
+++ js/login.js
@@ -12,15 +12,18 @@ function click() {
     var textoEmail = inputEmail.value
     var textoPass = inputPass.value
 
-    if (textoNome == ""){
-        alert("Nome Inválido tente Novamente")
+    if ((textoNome == "") && (textoEmail == "") && (textoPass == "")) {
+        alert("Informe algo!!!")
     }
-    if (textoEmail == ""){
-        alert("Email Inválido tente Novamente")
-    }
-    if (textoPass == ""){
-        alert("Senha Inválido tente Novamente")
-    }
+    // if (textoNome == ""){
+    //     alert("Nome Inválido tente Novamente")
+    // }else if (textoEmail == ""){
+    //     alert("Email Inválido tente Novamente")
+    // }else if (textoPass == ""){
+    //     alert("Senha Inválido tente Novamente")
+    // }else if((textoNome == "") && (textoEmail == "") && (textoPass == "")){
+    //     alert("Informe algo")
+    // }
     else{
         console.log(textoNome);
         console.log(textoEmail);
